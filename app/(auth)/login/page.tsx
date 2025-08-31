0d2b427d3c49778c2df19b57f961d9923284a304
ryku <najdhmadi@gmail.com>
2025-08-31 13:22:14 +0100

    delete readme

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,7 +2,13 @@
 export default function Page() {
   return (
     <div>
-      
+      <h1> Log in </h1>
+      <form >
+        <label > Email </label>
+        <input type="email" placeholder="example.gmail.com" />
+        <label> Password </label>
+        <input type="password" />
+      </form>
     </div>
   );
 }
